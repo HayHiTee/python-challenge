decbf0bec147ea85cf723da81eab344b1217b343
HayHiTee <youngest50@gmail.com>
2019-10-31 20:04:26 +0100

    Modified comment in config

--- app/service/resources/config.py
+++ app/service/resources/config.py
@@ -1,6 +1,7 @@
 from app.service.resources.resources import GitHubResource, BitBucketResource
 
-# This lists the resource classes to be used in AggregateResource
-# It must be a subclass of Resource and implement all the abstract methods
+# This defines the resource classes to be used in AggregateResource. It must be of dict type
+# The key is the name of the instance to be created and the value
+# must be a subclass of Resource from app.service.resources and implement all the abstract methods of the Resource
 
 resources_classes = {'github': GitHubResource, 'bitbucket': BitBucketResource}
